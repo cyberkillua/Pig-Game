91a3e2fac862eb7d55b1c5489f32658583fc962b
Moshood Alimi <moshood988@gmail.com>
2020-03-22 19:17:36 +0100

    Add files via upload
    new commit

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ document.querySelector(".btn-hold").addEventListener('click', function() {
     // move to the second players turn
      
     //change player whem it reaches 100
-    if(scores[activePlayer] >= 20) {
+    if(scores[activePlayer] >= 100) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!!';
         document.querySelector(".dice").style.display = "none";
         document.querySelector('.player-' + activePlayer +'-panel').classList.add('winner');
